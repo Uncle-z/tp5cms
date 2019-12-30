4b1d5806ddbf8235f22c6dbac0dfdf446c1fdb06
zouyinping <916103108@qq.com>
2019-12-30 23:28:03 +0800

    denglu

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -3,8 +3,8 @@ namespace app\admin\controller;
 
 use think\Controller;
 use think\View;
-//use app\admin\model\Admin;
-use app\admin\model\Category;
+use think\Request;
+use app\admin\model\Admin;
 
 /**
 * 登陆注册
@@ -12,20 +12,28 @@ use app\admin\model\Category;
 */
 class Login extends Controller
 {
-    
-    function __construct()
+    protected $request;
+
+    public function __construct(Request $request)
     {
-        # code...
+        $this->request = $request;
     }
     //注册
     public function register()
-    {
-        $request = request();      
-        if($request->isPost() && $request->param('do') == 'r'){
-            $info = Category::create(['name' => $request->param('username')]);
-            dump($info);
-            //$category->name = $request->param('username');
-            //$category->save();
+    {   
+        if($this->request->isPost()){
+            $hasname = Admin::where('username',$this->request->param('username'))->find();
+            if($hasname){
+               $this->error('用户名'. $this->request->param('username') .'已经存在', '/admin/login/register');
+                return;
+            }
+            if($this->request->param('password') === $this->request->param('pwdconfirm')){
+                $admin = new Admin($this->request->param());
+                $info = $admin->allowField(['username','password'])->save();
+                $this->success('注册成功,去登录！', 'admin/login/login');
+            }else{
+                $this->error('两次密码不相同', '/admin/login/register');
+            }           
         }else{
             return view('register');
         }       
@@ -33,11 +41,24 @@ class Login extends Controller
     //登陆
     public function login()
     {
-        // $info = db('admin')->where('userid',1)->find();
-        // dump($info);
-        //$admin = Admin::get(1);
-        //dump($admin->userid);
-        return view('login');
+        if($this->request->isPost()){
+            $user = new Admin();
+            // 查询单个数据
+            $info = $user->where('username', $this->request->param('username'))->find();
+            
+            if(!$info){
+               $this->error('用户名'. $this->request->param('username') .'不存在', '/admin/login/login');
+                return;
+            }
+            $info = $info->toArray();
+            if($this->request->param('password') === $info['password']){
+                $this->success('登录成功！', '/admin');
+            }else{
+                $this->error('密码错误，请重试', '/admin/login/login');
+            }
+        }else{
+            return view('login');
+        }
     }
 
     public function logout()
